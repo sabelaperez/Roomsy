7c6c96573526250bcecfff155b57c5e34db009c7
martindios <gonzalezdiosmartin@gmail.com>
2025-09-24 10:26:34 +0200

    initial commit

---
+++ Roomsy/src/main/java/com/roomsy/roomsy/RoomsyApplication.java
@@ -0,0 +1,13 @@
+package com.roomsy.roomsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsyApplication.class, args);
+	}
+
+}
