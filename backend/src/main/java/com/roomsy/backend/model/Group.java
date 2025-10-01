f0fb4114ae847f1b2763c6b0cd2d7ce1420ae28a
sabelapereez <sabelapereez@gmail.com>
2025-10-01 11:04:30 +0200

    Feat(model): Engadir modelo Group

--- backend/src/main/java/com/roomsy/backend/model/Group.java
+++ backend/src/main/java/com/roomsy/backend/model/Group.java
@@ -1,4 +1,39 @@
 package com.roomsy.backend.model;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "group")
 public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @Column
+    private String name;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp created_at;
+
+    @UpdateTimestamp
+    @Column
+    private Timestamp updated_at;
+
+    @Column(unique = true)
+    private String invite_code;
 }
+
