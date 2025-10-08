2008bfedb31397ad7b9524b3273d4e7c4d2067be
martindios <gonzalezdiosmartin@gmail.com>
2025-10-08 11:02:10 +0200

    add: some Repositories to the entities

---
+++ backend/src/main/java/com/roomsy/backend/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.roomsy.backend.repository;
+
+import com.roomsy.backend.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    Page<User> findByIsActive(boolean isActive, Pageable pageable);
+}
