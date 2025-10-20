02c096bc78ea51345d808332f830b4905e3c835c
martindios <gonzalezdiosmartin@gmail.com>
2025-10-20 17:12:26 +0200

    feat: continue implementing ExpenseService

--- backend/src/main/java/com/roomsy/backend/service/ExpenseService.java
+++ backend/src/main/java/com/roomsy/backend/service/ExpenseService.java
@@ -1,22 +1,28 @@
 package com.roomsy.backend.service;
 
+import com.roomsy.backend.exception.ResourceNotFoundException;
 import com.roomsy.backend.model.ExpenseItem;
+import com.roomsy.backend.model.SharedExpense;
 import com.roomsy.backend.repository.ExpenseItemRepository;
+import com.roomsy.backend.repository.SharedExpenseRepository;
 import jakarta.transaction.Transactional;
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
-public class ExpenseItemService {
+public class ExpenseService {
 
     private ExpenseItemRepository expenseItemRepository;
+    private SharedExpenseRepository sharedExpenseRepository;
 
     @Autowired
-    public ExpenseItemService(ExpenseItemRepository expenseItemRepository) {
+    public ExpenseService(ExpenseItemRepository expenseItemRepository,  SharedExpenseRepository sharedExpenseRepository) {
         this.expenseItemRepository = expenseItemRepository;
+        this.sharedExpenseRepository = sharedExpenseRepository;
     }
 
     @Transactional
@@ -25,16 +31,17 @@ public class ExpenseItemService {
     }
 
     @Transactional
-    public void deleteExpenseItem(@NonNull UUID id) throws Exception {
+    public void deleteExpenseItem(@NonNull UUID id) throws ResourceNotFoundException {
         if(!expenseItemRepository.existsById(id)) {
-            throw new IllegalArgumentException("ExpenseItem with that id does not exist");
+            throw new ResourceNotFoundException("ExpenseItem with that id does not exist");
         }
 
         expenseItemRepository.deleteById(id);
         // TODO: gasto compensatorio (no se elimina de la DB realmente, se revierte)
     }
 
-    //public ExpenseItem reassignExpenseItem()
+
+    //public List<SharedExpense> generateSplitExpenses()
 
 
 }
