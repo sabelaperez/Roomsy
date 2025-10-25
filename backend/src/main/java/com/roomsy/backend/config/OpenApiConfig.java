27914ea5d6d23697596f2dba7f7f44493d4ba40e
martindios <gonzalezdiosmartin@gmail.com>
2025-10-25 15:09:58 +0200

    feat: add config to openapi

--- backend/src/main/java/com/roomsy/backend/config/OpenApiConfig.java
+++ backend/src/main/java/com/roomsy/backend/config/OpenApiConfig.java
@@ -1,4 +1,41 @@
 package com.roomsy.backend.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Configuration
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI roomsyOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8080");
+        devServer.setDescription("Development Server");
+
+        Contact contact = new Contact();
+        contact.setEmail("gonzalezdiosmartin@gmail.com");
+        contact.setName("Roomsy Team");
+
+        License mitLicense = new License()
+                .name("MIT License")
+                .url("");
+
+        Info info = new Info()
+                .title("Roomsy Backend API")
+                .version("0.0.1-SNAPSHOT")
+                .contact(contact)
+                .description("API documentation for Roomsy backend application")
+                .license(mitLicense);
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(devServer));
+    }
 }
