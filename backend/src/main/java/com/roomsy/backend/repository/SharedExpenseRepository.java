b167e6c31181b024b6c2c56db84757c5f35153fc
martindios <gonzalezdiosmartin@gmail.com>
2025-11-19 10:19:45 +0100

    feat: (security) minor changes

--- backend/src/main/java/com/roomsy/backend/repository/SharedExpenseRepository.java
+++ backend/src/main/java/com/roomsy/backend/repository/SharedExpenseRepository.java
@@ -2,6 +2,7 @@ package com.roomsy.backend.repository;
 
 import com.roomsy.backend.model.Group;
 import com.roomsy.backend.model.SharedExpense;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,13 @@ import java.util.UUID;
 
 public interface SharedExpenseRepository   extends JpaRepository<SharedExpense, UUID> {
     List<SharedExpense> findByGroup(Group group);
-    boolean deleteByIdReturningBoolean(UUID id);
+    @Transactional
+    default boolean deleteByIdReturningBoolean(UUID id) {
+        if (existsById(id)) {
+            deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
